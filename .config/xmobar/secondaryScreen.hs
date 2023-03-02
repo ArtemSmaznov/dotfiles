6d5a1d54322c68c5c96137f874ca25070b06ffa6
Artem Smaznov <ArtemSmaznov@gmail.com>
2023-03-02 17:58:22 -0500

    chore(xmobar): change main font

--- .config/xmobar/secondaryScreen.hs
+++ .config/xmobar/secondaryScreen.hs
@@ -1,6 +1,6 @@
 Config {
    -- appearance
-     font            = "SF Pro Text Regular 11"
+     font            = "Hack Nerd Font 11"
    , additionalFonts = [ "Hack Nerd Font Mono 18"       -- 1
                        , "Hack Nerd Font Mono 20"       -- 2
                        , "Hack Nerd Font Mono 28"       -- 3
@@ -24,13 +24,13 @@ Config {
    , iconRoot = ".config/xmobar/xpm/"
    , commands =
         [ Run Com "echo" ["<fc=#7c6f64>|</fc>"] "sep" 36000
-        , Run Com "echo" ["<action=`rofi -show drun`> <fn=3><fc=#1793d1>\xf303</fc></fn></action>"] "logo" 36000
+        , Run Com "echo" ["<action=`rofi -show drun`><fn=3><fc=#1793d1>\xf303</fc></fn></action>"] "logo" 36000
         , Run Date
-          "%l:%M %p "
+          "%l:%M %p"
           "time" 10
         , Run UnsafeStdinReader
         , Run MPD
-          ["-t", "  <statei>  <fn=4><artist> - <title></fn> [<flags>] " -- ["-t", "<box type=Bottom width=2 mb=2 color=#fabd2f> <statei>  <artist> - <title> </box>"
+          ["-t", "  <statei> <fn=4><artist> - <title></fn> [<flags>]" -- ["-t", "<box type=Bottom width=2 mb=2 color=#fabd2f> <statei>  <artist> - <title> </box>"
                , "--"
                , "-P", "<fn=1></fn>" -- play icon
                , "-Z", "<fn=1></fn>" -- pause icon
@@ -43,22 +43,22 @@ Config {
           , ("jp" , "<fn=4><fc=#fbf1c7>日本</fc></fn>")
           ]
         , Run Com "echo" [" </action>"] "_ke" 3600
-        , Run Com "echo" ["<box type=Bottom width=2 mb=2 color=#fb4934><action=`alacritty -e sudo pacman -Syu`>  <fn=1>\xf0f3</fn>  "] "_us" 3600
+        , Run Com "echo" ["<box type=Bottom width=2 mb=2 color=#fb4934><action=`alacritty -e sudo pacman -Syu`> <fn=1>\xf0f3</fn> "] "_us" 3600
         , Run Com ".local/bin/dm-scripts/helpers/updates" [] "updates" 3600
-        , Run Com "echo" ["  </action></box>"] "_ue" 3600
+        , Run Com "echo" [" </action></box>"] "_ue" 3600
         , Run BatteryP ["cat /sys/class/sony_controller_battery_a0:ab:51:62:65:1d/capacity"]
           ["-t", "<acstatus><watts> <left>%"
                , "--low", "20"
                -- , "--"
                ] 100
         , Run DynNetwork
-          ["-t", "<box type=Bottom width=2 mb=2 color=#8ec07c>  <fn=1>\xf484</fn>  <rx> <fn=6>\xf309\xf30c</fn> <tx> </box>"
+          ["-t", "<box type=Bottom width=2 mb=2 color=#8ec07c> <fn=1>\xf484</fn> <rx> <fn=6>\xf309\xf30c</fn> <tx> </box>"
                , "-S", "True"
                , "--"
                , "--devices", "eno1,wlan0,enp2s0f0"
                ] 20
         , Run CoreTemp
-          ["-t", "<box type=Bottom width=2 mb=2 color=#d3869b><action=`alacritty -e htop`>  <fn=1>\xf85a</fn>  <core0>° "
+          ["-t", "<box type=Bottom width=2 mb=2 color=#d3869b><action=`alacritty -e htop`> <fn=1>\xf85a</fn> <core0>°"
                -- High CPU Temp
                , "-H", "70"
                , "-h", "#fb4934"
@@ -67,7 +67,7 @@ Config {
                , "-l", "#b8bb26"
                ] 20
         , Run Cpu
-          ["-t", "(<total>%)  </action></box>"
+          ["-t", "(<total>%) </action></box>"
                -- High CPU Load
                , "-H", "80"
                , "-h", "#fb4934"
@@ -76,10 +76,10 @@ Config {
                , "-l", "#b8bb26"
                ] 20
         , Run Memory
-          ["-t", "<box type=Bottom width=2 mb=2 color=#83a598><action=`alacritty -e htop`>  <fn=1>\xf2db</fn>  <used> M (<usedratio>%)  </action></box>"
+          ["-t", "<box type=Bottom width=2 mb=2 color=#83a598><action=`alacritty -e htop`> <fn=1>\xf2db</fn> <used> M (<usedratio>%) </action></box>"
                ] 20
         , Run Volume "default" "Master"
-          ["-t", "<box type=Bottom width=2 mb=2 color=#b8bb26><action=`alacritty -e alsamixer`>  <status>  <volume>%</action>  </box>"
+          ["-t", "<box type=Bottom width=2 mb=2 color=#b8bb26><action=`alacritty -e alsamixer`> <status> <volume>%</action> </box>"
                , "--"
                -- ON Icon
                , "-O", "<fn=2>\xfa7d</fn>"
@@ -89,12 +89,12 @@ Config {
                , "-c", "#fb4934"
                ] 10
         , Run Date
-          "<action=`emacsclient -c -a 'emacs' --eval '(cfw:open-org-calendar)'`>  <fn=1>\xf5f5</fn>  %a, %d %b %Y  </action>"
+          "<action=`emacsclient -c -a 'emacs' --eval '(cfw:open-org-calendar)'`><fn=1>\xf5f5</fn> %a, %d %b %Y</action>"
           "date" 3600
         , Run Uptime
-          ["-t", "<box type=Bottom width=2 mb=2 color=#fabd2f>  <fn=2>\xf0aa</fn>  <days>d <hours>h  </box>"
+          ["-t", "<box type=Bottom width=2 mb=2 color=#fabd2f> <fn=2>\xf0aa</fn> <days>d <hours>h </box>"
                ] 3600
         , Run Com ".config/xmobar/trayer-padding-icon.sh" [] "trayerpad" 20
 ]
-   , template = "%logo% %time% %sep% %UnsafeStdinReader% } { %mpd% %sep% %kbd% %sep% %default:Master% %uptime% %sep% %date% "
+   , template = " %logo%%time% %sep%%UnsafeStdinReader% } { %mpd% %sep% %kbd% %sep% %default:Master% %uptime% %sep% %date% "
    }
