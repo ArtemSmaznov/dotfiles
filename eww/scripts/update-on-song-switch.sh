a06e53374cfc92206a9722688a25f3954d25c8ba
Artem Smaznov <ArtemSmaznov@gmail.com>
2024-02-19 01:38:04 -0500

    fix(eww): media-player progress bar action

--- eww/scripts/update-on-song-switch.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-eww update music-song="$(~/.local/bin/get-music.sh song)"
-eww update music-title="$(~/.local/bin/get-music.sh title)"
-eww update music-artist="$(~/.local/bin/get-music.sh artist)"
-eww update music-album="$(~/.local/bin/get-music.sh album)"
-eww update music-album-cover-file="$(~/.local/bin/get-music.sh albumcover)"
-# eww update music-player-cover-color="$(~/.local/bin/get-music.sh albumcovercolor)"
